7834cf4e3a57dbb115cf6b5934cbb389f492278a
Santiago Vanegas Gil <savanegasg@gmail.com>
2016-04-02 22:49:16 -0500

    Adding scaffolded project structure using Yeoman

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('nowerClient')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
